73002b50553dfeed66df68af988d584ac13a8120
s5227111 <56020965+s5227111@users.noreply.github.com>
2023-04-18 23:37:28 +0100

    Adding button, redirect from Login page & Videos page

---
+++ themes/generatepress/videos.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name:  Videos
+ * @package Respected.org
+ */
+get_header(); ?>
+
+<div>
+    <script src="https://player.vimeo.com/api/player.js"></script>
+    <iframe src="https://player.vimeo.com/video/791240564?h=d5cf348f24&title=0&byline=0&portrait=0" 
+    width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" 
+    allowfullscreen></iframe>
+    <script>
+    var iframe = document.querySelector('iframe');
+    var player = new Vimeo.Player(iframe);
+    player.on('ended', function() {alert("test")});
+    </script>
+</div>
+
+<?php get_footer(); ?>
